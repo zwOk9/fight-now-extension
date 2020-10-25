ed465f6521ba4c240292437085713fb78daf2937
zwOk9 <salimloudement@gmail.com>
2020-10-25 02:51:52 +0100

    [FIGHT NOW] [NOTIFICATION] check date now and storage chrom

--- src/background.js
+++ src/background.js
@@ -1,23 +1,67 @@
-
 import store from './store'
-
 import _ from 'lodash'
 
-window.setInterval(() => {
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create('refresh', { periodInMinutes: 2 });
+});
+
+
+chrome.alarms.onAlarm.addListener(() => {
+  checkNotification ()
+});
+
+function checkNotification () {
   store.dispatch('getFights').then(() => {
     let fights = store.getters.fights
     _.forEach(fights, (fight) => {
+      
       let id = fight._id
       try {
         chrome.storage.local.get(id, (result) => {
-          if (_.isNil(result[id]._id)) {
-            if (new Date(fight.dateFight).getTime() > Date.now) {
-              chrome.storage.local.set({[id]: fight})
+          if (_.isNil(result[id]) && (new Date(fight.dateFight).getTime() + (5*60*60*1000)) <= Date.now()) {
+            return
+          }
+          let modelFightCompare = {
+            _id: '',
+            broadcaster: [],
+            dateFight: '',
+            links: [],
+            title: '',
+            typeFight: ''
+          }
+          let modelFightApi = {
+            _id: '',
+            broadcaster: [],
+            dateFight: '',
+            links: [],
+            title: '',
+            typeFight: ''
+          }
+          modelFightCompare =  (_.isNil(result) || _.isNil(result[id])) ? [] : _.pick(result[id], _.keys(modelFightCompare))
+          modelFightApi = _.pick(fight, _.keys(modelFightApi))
+          if (_.isNil(result) || _.isNil(result[id]) || _.isNil(result[id]._id)) {
+            if ((new Date(fight.dateFight).getTime()) > Date.now()) {
+              let modelFightStorage = {
+                _id: fight._id,
+                broadcaster: fight.broadcaster,
+                dateFight: fight.dateFight,
+                links: fight.links,
+                title: fight.title,
+                typeFight: fight.typeFight,
+                notification: false
+              }
+              chrome.storage.local.set({[id]: modelFightStorage})
             } 
+          } else if (!_.isEqual(modelFightApi, modelFightCompare)) {
+            chrome.storage.local.set({[id]: modelFightApi})
           } else {
-            if (new Date(result[id].dateFight).getTime() <= Date.now) {
+            if ((new Date(result[id].dateFight).getTime()) <= Date.now() && !result[id].notification) {
               showNotif(fight)
-              chrome.storage.local.remove([id])
+              result[id].notification = true
+              chrome.storage.local.set({[id]: result[id]})
+            } else if ((new Date(result[id].dateFight).getTime() + (5*60*60*1000)) <= Date.now()) {
+              clearStorage([id])
             }
           }
         })
@@ -27,14 +71,21 @@ window.setInterval(() => {
     
     })
   })
-}, 60000)
+}
 
 function showNotif (data) {
     let notification = new Notification(data.title, {
-      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      icon: '',
       body: 'Hey there! You\'ve been notified!',
     })
     notification.onclick = () => {
-      window.open('http://stackoverflow.com/a/13328397/1269037')
+      window.open('')
     }
+}
+
+
+function clearStorage (idStorage) {
+  chrome.storage.local.get(idStorage, (result) => {
+    delete result[idStorage]
+  })
 }
